fa976dbaa2c1447ab21725eb320fe8e0cb744713
gabomur88 <gmurillo.rivero@gmail.com>
2026-09-17 02:21:50 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Concha Culo Teta
+          Learn React Me Cambiaron
         </a>
       </header>
     </div>
